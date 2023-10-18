4c22e7f24706833e023b6c557716c392afb6f638
Pedro Nunes <pedro.nunes@southsystem.com.br>
2023-10-18 13:38:11 -0300

    feat: complemented types and interfaces

--- src/types/Pokemon.interface.ts
+++ src/types/Pokemon.interface.ts
@@ -1,5 +1,64 @@
+import { FetchBaseQueryMeta } from '@reduxjs/toolkit/query/react';
+
 export interface PokemonDetailInterface {
   id: string,
   name: string,
   image: string,
+  url?: string
+}
+
+export type PokemonListData = {
+  next: string | null;
+  previous: string | null;
+  results: PokemonDetailInterface[];
+};
+
+export type RTKFetch = {
+  error?: undefined;
+  data: PokemonListData;
+  meta?: FetchBaseQueryMeta | undefined;
+};
+
+export type PokemonDetailFromApi = {
+  id: string,
+  name: string,
+  sprites: { front_default: string },
+};
+
+type DefaultApiProviderProps = {
+  isLoading: boolean,
+  isError: boolean,
+};
+
+export type ListDataType = {
+  data: PokemonListData;
+};
+
+export type GetByNameDataType = {
+  data: PokemonCardInterface;
+};
+
+export type ListApiProps = DefaultApiProviderProps & ListDataType;
+export type GetByNameApiProps = DefaultApiProviderProps & GetByNameDataType;
+
+export type PokemonOriginalStats = {
+  base_stat: number,
+  stat: { name: string }
+};
+
+export type PokemonStats = {
+  hp: number,
+  attack: number,
+  defense: number,
+  speed: number,
+  specialAttack: number,
+};
+
+export interface PokemonCardInterface {
+  id: number,
+  name: string,
+  abilities: object[],
+  sprites: { front_default: string },
+  types: object[],
+  stats: PokemonStats
 }
