edf96e4dcccf1192bbacb66c1e56e533719b7e46
Pedro Nunes <pedro.nunes@southsystem.com.br>
2023-10-18 13:38:58 -0300

    feat: added global style

---
+++ src/app/styles/globalStyle.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    background: linear-gradient(135deg, #FF9800 0%, #E64A19 100%);
+  }
+`
+
+export default GlobalStyle;
